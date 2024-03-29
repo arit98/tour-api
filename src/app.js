d2eaa386e0bd7236d6337d6b3c4a55c98551bccd
arit98 <aritradaschowdhury@gmail.com>
2024-03-30 02:06:02 +0530

    final build

--- src/app.js
+++ src/app.js
@@ -15,6 +15,13 @@ app.use(cors({
     credentials: true 
 }))
 
+app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
